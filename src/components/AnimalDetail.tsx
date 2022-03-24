f8b886479f77c9777ee0a941eab9a710c4d7c495
Mari-Artho <mari.artho@medieinstitutet.se>
2022-03-24 21:25:32 +0100

    Create feedStatus method.

--- src/components/AnimalDetail.tsx
+++ src/components/AnimalDetail.tsx
@@ -8,100 +8,73 @@ import { Link } from "react-router-dom";
 
 export function AnimalDetail() {
     const { id } = useParams();
-    //fetch data
     //const [data, setData] = useState<Animal[]>([]);
     const [ feed, setFeed] = useState(false);
-
-//     useEffect(()=>{
-//     //localstorage
-//     let getData = localStorage.getItem('data');
-//     let fetchData = async()=>{
-//         let result = await axios(
-//         'https://animals.azurewebsites.net/api/animals'
-//         );
-//         setData(result.data);
-//         localStorage.setItem('data', JSON.stringify(result));
-//     }; 
-//     fetchData();
-// }, []); 
 
 //filter by id
 //parseInt is convert a string integer to a numeric integer
 //(id!) tells TypeScript that even though something looks like it could be null, it can trust you that it's not.
 let dataJSON = localStorage.getItem("data");
 let data: Animal[] = JSON.parse(dataJSON!);
-let animals = data.filter(idFromData => {
+let animal = data.find(idFromData => {
     return idFromData.id == parseInt(id!);
   });
-  console.log("resの中身 ", animals);
+  console.log("animalaの中身 ", animal);
+  if (animal == null) {
+     return (<p>Not found</p>);
+}
 
  //disable button
- const [once, setOnce] = useState(false);
-
-//  //localstorage
-//  useEffect(()=>{
-//      let dataFromLS = localStorage.getItem('data');
-//      if(dataFromLS){
-//          setData(JSON.parse(dataFromLS));
-//      }
-//  }, [animalId]);
+ //const [once, setOnce] = useState(false);
 
 //hungry or full?
-  let Hungry =  data.map(time =>{
+function CheckHungry() {
       let timeSinceLastFed = Math.floor((new Date().getTime()
-      - new Date(time.lastFed).getTime())/(1000*60*60));
+      - new Date(animal!.lastFed).getTime())/(1000*60*60));
       console.log(timeSinceLastFed);
 
-      let test = ()=>{if(timeSinceLastFed>=4){
-        animals.map((item)=>{
-              item.isFed = false;
+      if(timeSinceLastFed>=4){
+        animal!.isFed = false;
               return(
                   <>
                   <h1>I haven't been fed for more than 4 hours</h1>
-                  <h1>{time.isFed}</h1>
-                  <h1>{item.isFed}</h1>
                   </>
               )
-          })
       }else{
           return <h1>I am full!!!!</h1>
       }
-    }; return (<>{test}</>)
-});
+    return (<>{test}</>)
+}
 
-//toggle button
-function feedStatus(){
+//let Hungry = { checkHungry };
+
+//feed status button
+function feedStatus(animal: Animal, data: Animal[]){
     console.log("You clicked a button")
-    animals.map(resData=>{
-        resData.lastFed = new Date();
-        setFeed(!resData);
-    });
+    animal.lastFed = new Date();
+    animal.isFed = true;
     localStorage.setItem('data', JSON.stringify(data));
 }
 
 //I got an error, so I added this '{res.toString()}' to solve it.
 return(
     <Fragment>
-        <h2>Animal ID:  {id} </h2>
-        <button onClick={feedStatus} disabled={once} >I am hungry</button>
+        <button onClick={()=>feedStatus(animal!, data)} >I am hungry</button>
         {!feed && <p>Not feed yet!!!</p>}
         {feed && <p>Fed is done!</p>}
 
-        <div>Check more than 4hours here →→{Hungry}</div>
-        {Hungry}
-        <ul>
-            {animals.map(item=>(
-                <li key={item.id}>
-                    <h1>Name: {item.name}</h1>
-                    <img src={item.imageUrl} width="140px" height='100px' />
-                    <p>Year of birth: {item.yearOfBirth}</p>
-                    <p>Long description: <br></br> {item.longDescription}</p>
-                    <p>Medicine: {item.medicine}</p>
-                    {!item.isFed && <p>I'm Hungry!</p>}
-            
-                    <p>Lastfed: {item.lastFed}</p>
-                </li>
-            ))}
+        <div>Check more than 4hours here →→{CheckHungry}</div>
+        
+         <ul>
+           <li key={animal.id}>
+             <h1>Name: {animal.name}</h1>
+             <img src={animal.imageUrl} width="140px" height='100px' />
+             <p>Year of birth: {animal.yearOfBirth}</p>
+             <p>Long description: <br></br> {animal.longDescription}</p>
+             <p>Medicine: {animal.medicine}</p>
+             {!animal.isFed && <p>I'm Hungry!</p>}
+             <p>Lastfed: {animal.lastFed}</p>
+            </li>
         </ul>
         <br></br>
         <button style={{marginLeft:"40%"}}><Link to={'/'}>Back to ZOO</Link></button>
